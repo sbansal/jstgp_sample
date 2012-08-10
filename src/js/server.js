acc7b703a6f7ef2da19de06bdeb3379edfca01ce
Stephen Starkey <sstarkey@backstopsolutions.com>
2012-08-10 13:57:53 -0500

    Using express now instead.

--- src/js/server.js
+++ src/js/server.js
@@ -1,6 +1,9 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
