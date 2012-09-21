a2491b09bc070d4eacac826435bb2b28914b8a93
Jake Scruggs <jscruggs@backstopsolutions.com>
2012-09-21 15:59:32 -0500

    trying and failing to get out first quiz to display on the webserver

--- whoami/whoami-test.js
+++ whoami/whoami-test.js
@@ -24,15 +24,28 @@ vows.describe('The Who am I Engine').addBatch({
     'should have a web server': function(topic){
 	    assert.isNotNull(topic.server);
     },
-    'should be able to stop the web server': function(topic){
-	    var serverStatus = 'alive?';
-	    topic.server.close(function () { 
-			console.log(serverStatus);
-		    serverStatus = 'dead';
-			console.log(serverStatus);
-	    });
-	    assert.equal(serverStatus,'dead');
-    }
-
+    // 'should be able to stop the web server': function(topic){
+    //      var serverStatus = 'alive?';
+    // 
+    //      topic.server.close(function () {
+    //       serverStatus = 'dead';
+    //      });
+    // 
+    //      setTimeout(function(){assert.equal(serverStatus,'dead');},250);     
+    // },
+    'should be able to stop the web server but not be insane': function(topic){
+        assert.equal(topic.server.close().connections,0);        
+    },
+    'should generate a character quiz snippet': function(topic){
+      assert.match(topic.getQuizSnippet(0), /Jscruggs.jpg/);
+      assert.match(topic.getQuizSnippet(0), /input/);
+      assert.match(topic.getQuizSnippet(0), /img src/);
+    },
+    'should tell us if the correct name passes a quiz': function(topic){
+      assert.isTrue(topic.answerQuizSnippet(0,'Jake Scruggs'));
+    },
+    'should tell us if the wrong name fails the quiz': function(topic){
+      assert.isFalse(topic.answerQuizSnippet(0,'Frank O\'Hara'));
+    },
   }
 }).export(module);
--- whoami/whoami.js
+++ whoami/whoami.js
@@ -1,15 +1,20 @@
 var http = require('http');
 
 exports.Whoami = function() {
+  var that = this;
+  
   this.backstoppers = [];
   this.server = 
 	http.createServer(function (req, res) {
+    this.htmlResponseBody = '<HTML><HEAD><TITLE>Hello World</TITLE></HEAD>';
+    this.htmlResponseBody += '<BODY>';
+    this.htmlResponseBody += exports.Whoami.getQuizSnippet(0);
+    this.htmlResponseBody += '</BODY></HTML>';
+
 		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.end('<HEAD><TITLE>Hello World</TITLE></HEAD><BODY>mooooO!</body>');
+		res.end(this.htmlResponseBody);
 	}).listen(1337, '127.0.0.1');
 }
-
-
 
 exports.Whoami.prototype = {
   getStatus: function () {
@@ -28,6 +33,18 @@ exports.Whoami.prototype = {
             image_url: "https://wiki/w/images/9/98/Jscruggs.jpg"}]
   },
   getSampleHtml: function() {
-    return '<HEAD><TITLE>Hello World</TITLE></HEAD><BODY>mooooO!</body>';
+    return '<HEAD><TITLE>Hello World</TITLE></HEAD><BODY>mooooO!</BODY>';
+  },
+  getQuizSnippet: function(index) {
+    var quizHtml;
+    
+    quizHtml = '<div><img src=' + this.backstoppers[index].image_url + '/><input type="text" name="name"/></div>';
+    
+    return quizHtml;
+  },
+  answerQuizSnippet: function(index, answer){
+    return this.backstoppers[index].name === answer;    
   }
 };
+
+exports.Whoami();
