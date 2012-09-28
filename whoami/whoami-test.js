f5f8e5d5cc9783c516c48ab2940bf1c25e6f885c
Shubham Bansal <shubhambansal15@gmail.com>
2012-09-28 15:33:29 -0500

    add currying functionality, rip apart server we made a curriable function that is passed into node's server to process requests, this should allow us to easily test the request processing independent of the node server actually running

--- whoami/whoami-test.js
+++ whoami/whoami-test.js
@@ -34,7 +34,8 @@ vows.describe('The Who am I Engine').addBatch({
     //      setTimeout(function(){assert.equal(serverStatus,'dead');},250);     
     // },
     'should be able to stop the web server but not be insane': function(topic){
-        assert.equal(topic.server.close().connections,0);        
+		topic.startServer();		
+       	setTimeout(function(){assert.equal(topic.server.close().connections,0);},100000);        
     },
     'should generate a character quiz snippet': function(topic){
       assert.match(topic.getQuizSnippet(0), /Jscruggs.jpg/);
@@ -46,6 +47,6 @@ vows.describe('The Who am I Engine').addBatch({
     },
     'should tell us if the wrong name fails the quiz': function(topic){
       assert.isFalse(topic.answerQuizSnippet(0,'Frank O\'Hara'));
-    },
+    }
   }
 }).export(module);
--- whoami/whoami.js
+++ whoami/whoami.js
@@ -1,50 +1,75 @@
 var http = require('http');
 
-exports.Whoami = function() {
-  var that = this;
-  
-  this.backstoppers = [];
-  this.server = 
-	http.createServer(function (req, res) {
-    this.htmlResponseBody = '<HTML><HEAD><TITLE>Hello World</TITLE></HEAD>';
-    this.htmlResponseBody += '<BODY>';
-    this.htmlResponseBody += exports.Whoami.getQuizSnippet(0);
-    this.htmlResponseBody += '</BODY></HTML>';
+var wtf = '?';
 
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.end(this.htmlResponseBody);
-	}).listen(1337, '127.0.0.1');
-}
-
-exports.Whoami.prototype = {
-  getStatus: function () {
-    return 'not written';
-  },
-  getBackstoppers: function() {
-    return this.backstoppers;
-  },
-  loadFakeBackstoppers: function() {
-    this.backstoppers = [
-        {name: "Jake Scruggs", 
-            image_url: "https://wiki/w/images/9/98/Jscruggs.jpg"},
-        {name: "Bob Scruggs", 
-            image_url: "https://wiki/w/images/9/98/Jscruggs.jpg"},
-        {name: "Steve Scruggs", 
-            image_url: "https://wiki/w/images/9/98/Jscruggs.jpg"}]
-  },
-  getSampleHtml: function() {
-    return '<HEAD><TITLE>Hello World</TITLE></HEAD><BODY>mooooO!</BODY>';
-  },
-  getQuizSnippet: function(index) {
-    var quizHtml;
-    
-    quizHtml = '<div><img src=' + this.backstoppers[index].image_url + '/><input type="text" name="name"/></div>';
-    
-    return quizHtml;
-  },
-  answerQuizSnippet: function(index, answer){
-    return this.backstoppers[index].name === answer;    
-  }
+Function.prototype.method = function (name, func) {
+    this.prototype[name] = func;
+    return this;
 };
 
-exports.Whoami();
+Function.method('curry',function () {
+	var slice = Array.prototype.slice, 
+		args = slice.apply(arguments),
+		that = this;
+	return function() {
+		return that.apply(null, args.concat(slice.apply(arguments)));
+	};
+})
+
+exports.Whoami = function() {
+	this.backstoppers = [];
+	this.server;
+}
+
+	exports.Whoami.prototype = {
+		
+		
+		getQuizSnippet: function(index) {
+			var quizHtml;
+
+			quizHtml = '<div><img src="' + this.backstoppers[index].image_url + '"/><input type="text" name="name"/></div>';
+
+			return quizHtml;
+		},
+		requestProcessor: function(that, req, res) {						
+			if (/answer/.test(req.url)){
+				
+			} else {
+				this.htmlResponseBody = '<HTML><HEAD><TITLE>Hello World</TITLE></HEAD>';
+				this.htmlResponseBody += '<BODY>';
+				this.htmlResponseBody += that.getQuizSnippet(0);
+				this.htmlResponseBody += '</BODY></HTML>';
+
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.end(this.htmlResponseBody);
+			}		
+			
+		},
+		startServer: function() {			
+			var that=this;
+			this.server = http.createServer(this.requestProcessor.curry(that)).listen(31337, '127.0.0.1');			
+		},
+		getStatus: function () {
+			return 'not written';
+		},
+		getBackstoppers: function() {
+			return this.backstoppers;
+		},
+		loadFakeBackstoppers: function() {
+			this.backstoppers = [
+			{name: "Jake Scruggs", 
+			image_url: "https://wiki/w/images/9/98/Jscruggs.jpg"},
+			{name: "Bob Scruggs", 
+			image_url: "https://wiki/w/images/9/98/Jscruggs.jpg"},
+			{name: "Steve Scruggs", 
+			image_url: "https://wiki/w/images/9/98/Jscruggs.jpg"}]
+		},
+		getSampleHtml: function() {
+			return '<HEAD><TITLE>Hello World</TITLE></HEAD><BODY>mooooO!</BODY>';
+		},
+		answerQuizSnippet: function(index, answer){
+			return this.backstoppers[index].name === answer;    
+		}
+	};
+
+	exports.Whoami();
